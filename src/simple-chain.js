e7d890b9a453057851ebdc57bf64e376d3edbeca
yivanova <yuliya.maslenka@gmail.com>
2021-09-17 17:27:24 +0200

    resolved task simple-chain.js

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,28 +1,32 @@
-import { NotImplementedError } from '../extensions/index.js';
-
 /**
  * Implement chainMaker object according to task description
- * 
+ *
  */
 export default {
+  array: [],
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.array.length
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+    this.array.push(`( ${value} )`)
+    return this
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    if (typeof position !== 'number' || position <= 0 || position > this.array.length){
+      this.array = [];
+      throw new Error('You can\'t remove incorrect link!');
+    } else {
+      this.array.splice(position -1, 1)
+    }
+    return this
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.array.reverse()
+    return this
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let chain = this.array.join('~~');
+    this.array = []
+    return chain
   }
 };
